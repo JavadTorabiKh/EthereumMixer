ee1e82ed79e5605c8a676032a8bb352f60c48df1
javad <j.2528840@gmail.com>
2025-04-15 10:29:32 +0330

    fix

--- token/mixer.sol
+++ token/mixer.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 contract MyContract { 
     address payable private recipient;
+    
     constructor() {
 
         recipient =  payable (0x4931d8d5c783Fb589528f8657FF0DFB4dCD8be68);
